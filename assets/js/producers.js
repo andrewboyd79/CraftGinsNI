2781e0a487e6101250133f1be7f4cef2eee0fe9d
andrewboyd79 <andrewboyd79@googlemail.com>
2021-03-07 22:52:04 +0000

    button functions rewritten to tidy up jQuery syntax and make use of category key added to JSON file

--- assets/js/producers.js
+++ assets/js/producers.js
@@ -16,7 +16,8 @@ function appendData (data) {
             
 
                 //declares the variables & creates each element
-                var box = document.createElement("div"); 
+                var box = document.createElement("div");
+                var location = document.createElement("div"); 
                 var image = document.createElement("img"); 
                 var headingtext = document.createElement("h2");
                 var categorytext = document.createElement("h6"); 
@@ -27,25 +28,32 @@ function appendData (data) {
             if (data[i].category === "Producer")  { //if statement checks if condition is met
                 //assigns id/classes to the elements
                 box.id=`${data[i].title}`;
-                box.className= "producerbox col-6";
+                box.className= `${data[i].county} producerbox col-6`;
+                location.className="countyheading";
+                console.log("Location div created");
                 headingtext.className = "producerheading";
                 categorytext.className = "producerheading"; 
                 bodytext.className = "producerbody"; 
                 image.className = "producerimage"; 
                 info.className = "infobutton";  
-                nearby.className="nearbybutton";}
-            else{
+                nearby.className="nearbybutton";
+                }
+
+            else { //else if condition is not met
+                //assigns id/classes to the elements
                 box.id=`${data[i].title}`;
-                box.className= "barbox col-6";
+                box.className= `${data[i].county} barbox col-6`;
+                location.className="countyheading";
                 headingtext.className = "barheading";
                 categorytext.className = "barheading"; 
                 bodytext.className = "barbody"; 
                 image.className = "barimage"; 
                 info.className = "infobutton";  
                 nearby.className="nearbybutton";
-            }
+                }
 
                 //adds the informtion from the .json file to the relevant element
+                location.innerHTML=`${data[i].county}`;
                 image.src = `${data[i].image}`; 
                 headingtext.innerHTML = `${data[i].title}`;
                 categorytext.innerHTML=`${data[i].category}`             
@@ -55,73 +63,44 @@ function appendData (data) {
 
                 // appends the individual child divs to the parent and then the elements the box div
                 mainContainer1.append(box); 
-                box.append(image, headingtext, bodytext, info, nearby);
+                box.append(location,image, headingtext, bodytext, info, nearby);
                 headingtext.append(categorytext);
                 }
         }
 
+$("#antdownbutton").click(function(){ // selects the button with antdownbutton ID and runs the following function when button is clicked
+    if($(".Antrim, .Down")){ // if class is Antrim or Down
+        $(".Tyrone,.Fermanagh, .Armagh, .Derry").hide(); // div with classes Tyrone, Fermanagh, Armagh, Derry are hidden
+    }  
+})
 
-// selects the button with antdownbutton ID and runs the following function when button is clicked
-$("#antdownbutton").click(function(){
+$("#tyrfermbutton").click(function(){ // selects the button with tyrfermbutton ID and runs the following function when button is clicked
+    if($(".Tyrone, .Fermanagh")){ // if class is Tyrone or Fermanagh
+        $(".Down, .Antrim, .Armagh, .Derry").hide(); // div with classes Down, Antrim, Armagh, Derry are hidden
+    }  
+})
 
-    //the divs with the ID containing 'Jawbox & Ruth' are shown
-    $("div[id*='Jawbox']").show();
-    $("div[id*='Copeland']").show();
+$("#armderbutton").click(function(){ // selects the button with armderbutton ID and runs the following function when button is clicked
+    if($(".Armagh, .Derry")){ // if class is Armagh or Derry
+        $(".Antrim,.Down, .Tyrone, .Fermanagh").hide(); // div with classes Antrim, Down, Tyrone, Fermanagh are hidden
+    }  
+})
 
-    //the remaining divs are hidden
-    $("div[id*='Boatyard Gin']").hide();
-    $("div[id*='Symphonia Gin']").hide();
-    $("div[id*='Echlinville']").hide();
-    $("div[id*='Belfast']").hide();
-    $("div[id*='Shortcross']").hide();    
-    }
-)
-
-// selects the button with tyrfermbutton ID and runs the following function when button is clicked
-$("#tyrfermbutton").click(function(){
-
-    //the divs with the ID containing 'Boatyard Gin & 4' are shown
-    $("div[id*='Boatyard Gin']").show();
-    $("div[id*='Symphonia Gin']").show();
-    
-    //the remaining divs are hidden
-    $("div[id*='Jawbox']").hide();
-    $("div[id*='Copeland']").hide();
-    $("div[id*='Echlinville']").hide();
-    $("div[id*='Belfast']").hide();
-    $("div[id*='Shortcross']").hide(); 
-    }
-)
-
-// selects the button with armderbutton ID and runs the following function when button is clicked
-$("#armderbutton").click(function(){
-
-    //the divs with the ID containing 'Producer 5 & 6' are shown
-    $("div[id*='Echlinville']").show();
-    $("div[id*='Belfast']").show();
-    $("div[id*='Shortcross']").show();
-
-    //the remaining divs are hidden
-    $("div[id*='Jawbox']").hide();
-    $("div[id*='Copeland']").hide();
-    $("div[id*='Boatyard Gin']").hide();
-    $("div[id*='Symphonia Gin']").hide();
-    }
-)
-
-// selects the button with showallbutton ID and runs the following function when button is clicked
-$("#showallbutton").click(function(){
+$("#showallbutton").click(function(){ // selects the button with showallbutton ID and runs the following function when button is clicked
     if($("div:contains('box')")){ // if div contains word box then
-        $(".barbox").show(); // elements with barbox class are shown
-        $(".producerbox").show(); // elements with the producer boix class are shown
-        }      
-    }
-)
+        $(".barbox, .producerbox").show(); // elements with producerbox and barbox class are shown
+    }      
+})
 
-// selects the button with producronlybutton ID and runs the following function when button is clicked
-$("#produceronlybutton").click(function(){
+$("#produceronlybutton").click(function(){ // selects the button with producronlybutton ID and runs the following function when button is clicked
     if($("h2:not('producerbox')")){ // if h2 isn't producerbox
         $(".barbox").hide(); // elements with the barbox class are hidden - therefore displaying producer only
     }
-});
+})
+
+$("#ginjointsonlybutton").click(function(){ // selects the button with producronlybutton ID and runs the following function when button is clicked
+    if($("h2:not('barbox')")){ // if h2 isn't producerbox
+        $(".producerbox").hide(); // elements with the barbox class are hidden - therefore displaying producer only
+    }
+})
 
