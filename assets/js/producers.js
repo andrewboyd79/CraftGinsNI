03461a1e46ec6dcacee15d00bfacc48e124ae6aa
andrewboyd79 <andrewboyd79@googlemail.com>
2021-03-07 19:48:46 +0000

    Added additional javascript on appending json data on the webpage and functions associated with filtering using the buttons

--- assets/js/producers.js
+++ assets/js/producers.js
@@ -1,81 +1,54 @@
-$(document).ready(function() {			
-    $("#getdata").click(function(event){
-        $.getJSON('producers.json', function(data) {
-                   
-         }
-        );
-
-
-
-/*$('#demo').html(`<div id="wrapperdiv"></div>`);
-                   for (var i=0; i< data.length; i++);
-                    
-    $('#wrapperdiv').html(`<div id = "${data[i].title}" class="producerbox col-6"></div>`);
-    $('.producerbox').html(`<img src='${data[i].image}' class="producerimage"><img>`);
-    $('.producerbox').append(`<h2 class="producerheading"> ${data[i].title} </h2>`);
-    $('.producerbox').append(`<div class="producerbody"> ${data[i].overview} </div>`);
-    $('.producerbox').append(`<button class="infobutton"> More Information </button>`);
-    $('.producerbox').append(`<button class="nearbybutton"> Nearby </button>`);}
-               });
-            });
-		*/		
-
-
-
-/*$(document).ready(function(){
-    $('#getdata').click(function(event){
-       $.getJSON('producers.json', function(data) {
-           $('#test').html(`<div id="wrapperdiv"> data.title  </div>`); 
-      }
-})*/
-
-/*$('#getdata').click(function(event){
-    $('#demo').html(`<div id="wrapperdiv"> data.title  </div>`);
-    $('#wrapperdiv').html(`<div class="producerbox col-6"></div>`);
-    $('.producerbox').html(`<img src='assets/images/LogoCropped.png' class="producerimage"><img>`);
-    $('.producerbox').append(`<h2 class="producerheading"> Test Heading </h2>`);
-    $('.producerbox').append(`<div class="producerbody"> Test Div </div>`);
-    $('.producerbox').append(`<button class="infobutton"> More Information </button>`);
-    $('.producerbox').append(`<button class="nearbybutton"> Nearby </button>`);
-
-    }
-)*/
-
-/*fetch('producers.json') //fetches the producers.json file
+fetch('producers.json') //fetches the producers.json file
 .then(function(response){
     return response.json(); 
 })
 .then(function(data){
+    //console.log(data);
     appendData(data);
 })
 ;
 
 //function to append the data from JSON file
 function appendData (data) {
-    var mainContainer1=document.getElementsByClassName("demo")[0]; //fetches the div with the ID demo1
+    console.log(data);
+    var mainContainer1=document.getElementsByClassName("datawrapper")[0]; //fetches the div with the ID demo1
         for (var i=0; i< data.length; i++) { //loops through every object in the JSON
-            if (data[i].title !== "X") { //if statement checks if condition is met
+            
 
                 //declares the variables & creates each element
                 var box = document.createElement("div"); 
                 var image = document.createElement("img"); 
-                var headingtext = document.createElement("h2"); 
+                var headingtext = document.createElement("h2");
+                var categorytext = document.createElement("h6"); 
                 var bodytext = document.createElement("div"); 
                 var info = document.createElement("button"); 
-                var nearby = document.createElement("button"); 
+                var nearby = document.createElement("button");
 
+            if (data[i].category === "Producer")  { //if statement checks if condition is met
                 //assigns id/classes to the elements
                 box.id=`${data[i].title}`;
                 box.className= "producerbox col-6";
-                headingtext.className = "producerheading"; 
+                headingtext.className = "producerheading";
+                categorytext.className = "producerheading"; 
                 bodytext.className = "producerbody"; 
                 image.className = "producerimage"; 
                 info.className = "infobutton";  
+                nearby.className="nearbybutton";}
+            else{
+                box.id=`${data[i].title}`;
+                box.className= "barbox col-6";
+                headingtext.className = "barheading";
+                categorytext.className = "barheading"; 
+                bodytext.className = "barbody"; 
+                image.className = "barimage"; 
+                info.className = "infobutton";  
                 nearby.className="nearbybutton";
+            }
 
                 //adds the informtion from the .json file to the relevant element
                 image.src = `${data[i].image}`; 
-                headingtext.innerHTML = `${data[i].title}`;             
+                headingtext.innerHTML = `${data[i].title}`;
+                categorytext.innerHTML=`${data[i].category}`             
                 bodytext.innerHTML = `${data[i].overview}`; 
                 info.innerHTML = `More Information`;
                 nearby.innerHTML = `Nearby`;
@@ -83,9 +56,9 @@ function appendData (data) {
                 // appends the individual child divs to the parent and then the elements the box div
                 mainContainer1.append(box); 
                 box.append(image, headingtext, bodytext, info, nearby);
+                headingtext.append(categorytext);
                 }
         }
-}*/
 
 
 // selects the button with antdownbutton ID and runs the following function when button is clicked
@@ -98,8 +71,9 @@ $("#antdownbutton").click(function(){
     //the remaining divs are hidden
     $("div[id*='Boatyard Gin']").hide();
     $("div[id*='Symphonia Gin']").hide();
-    $("div[id*='Producer 5']").hide();
-    $("div[id*='Producer 6']").hide();    
+    $("div[id*='Echlinville']").hide();
+    $("div[id*='Belfast']").hide();
+    $("div[id*='Shortcross']").hide();    
     }
 )
 
@@ -113,8 +87,9 @@ $("#tyrfermbutton").click(function(){
     //the remaining divs are hidden
     $("div[id*='Jawbox']").hide();
     $("div[id*='Copeland']").hide();
-    $("div[id*='Producer 5']").hide();
-    $("div[id*='Producer 6']").hide();
+    $("div[id*='Echlinville']").hide();
+    $("div[id*='Belfast']").hide();
+    $("div[id*='Shortcross']").hide(); 
     }
 )
 
@@ -122,8 +97,9 @@ $("#tyrfermbutton").click(function(){
 $("#armderbutton").click(function(){
 
     //the divs with the ID containing 'Producer 5 & 6' are shown
-    $("div[id*='Producer 5']").show();
-    $("div[id*='Producer 6']").show();
+    $("div[id*='Echlinville']").show();
+    $("div[id*='Belfast']").show();
+    $("div[id*='Shortcross']").show();
 
     //the remaining divs are hidden
     $("div[id*='Jawbox']").hide();
@@ -135,13 +111,17 @@ $("#armderbutton").click(function(){
 
 // selects the button with showallbutton ID and runs the following function when button is clicked
 $("#showallbutton").click(function(){
-
-    //all divs are shown
-    $("div[id*='Producer 5']").show();
-    $("div[id*='Producer 6']").show();
-    $("div[id*='Jawbox']").show();
-    $("div[id*='Copeland']").show();
-    $("div[id*='Boatyard Gin']").show();
-    $("div[id*='Symphonia Gin']").show();    
+    if($("div:contains('box')")){ // if div contains word box then
+        $(".barbox").show(); // elements with barbox class are shown
+        $(".producerbox").show(); // elements with the producer boix class are shown
+        }      
     }
 )
+
+// selects the button with producronlybutton ID and runs the following function when button is clicked
+$("#produceronlybutton").click(function(){
+    if($("h2:not('producerbox')")){ // if h2 isn't producerbox
+        $(".barbox").hide(); // elements with the barbox class are hidden - therefore displaying producer only
+    }
+});
+
