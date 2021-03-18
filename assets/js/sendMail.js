f0a042d0a9cd09b626c95b35d59b6d354f993abb
andrewboyd79 <andrewboyd79@googlemail.com>
2021-03-18 21:32:07 +0000

    Code beautified

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -1,17 +1,21 @@
 function sendMail(contactForm) {
-   emailjs.send("service_mcfsn9l", "template_ilc3tsk", { //passes in the service name, template ID and template paramters
-        "from_name": document.getElementById('fromName').value, //specifies template parameters
-       "from_email": document.getElementById('fromEmail').value,
-       "message": document.getElementById('msg').value
-   })
-   .then(
-       function(response) {
-           alert("Thank you for contacting us!"); //displays alert message when successful
-       },
-       function(error) {
-        alert("Unfortunately there has been an error with your submission. Please try again later!"); //displays an alert message when unsuccessful
-       }
-   );
-   return false;  // To block from loading a new page
-}
-
+    emailjs.send("service_mcfsn9l",
+      "template_ilc3tsk", { //passes in the service name, template ID and template paramters
+        "from_name": document.getElementById('fromName')
+        .value, //specifies template parameters
+        "from_email": document.getElementById('fromEmail').value,
+        "message": document.getElementById('msg').value
+      })
+      .then(
+        function(response) {
+          alert(
+          "Thank you for contacting us!"); //displays alert message when successful
+        },
+        function(error) {
+          alert(
+            "Unfortunately there has been an error with your submission. Please try again later!"
+            ); //displays an alert message when unsuccessful
+        }
+      );
+    return false; // To block from loading a new page
+  }
