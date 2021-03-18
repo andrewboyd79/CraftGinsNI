0cea045de216981a2873f34bf3c02b16223c38ae
andrewboyd79 <andrewboyd79@googlemail.com>
2021-03-18 21:30:53 +0000

    Changed to for of loop

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,5 +1,5 @@
 //code taken from https://stackoverflow.com/questions/15531390/adding-array-of-markers-in-google-map and amended for purpose
-function initMap (testLocations) { 
+function initMap (locations) { //data will not be populated until dropdown is selected
 
    //creates new map, sets relevant options and places it in the 'map' div element
    let map = new google.maps.Map(document.getElementById('map'), { 
@@ -13,16 +13,16 @@ function initMap (testLocations) {
    let marker, i;
 
    //iterates through the locations variable to create markers and position them on the map
-  for (i=0; i<testLocations.length;i++){  
+  for (let location of locations){  
       marker = new google.maps.Marker({ 
          //position: new google.maps.LatLng(locations[i][1],locations[i][2]),
-         position: new google.maps.LatLng(testLocations[i].lat,testLocations[i].lng),
+         position: new google.maps.LatLng(location.lat,location.lng),
          map: map 
    });
    
    google.maps.event.addListener(marker, 'click', (function(marker, i) {
          return function() {
-            infowindow.setContent(testLocations[i].title);
+            infowindow.setContent(location.title);
             infowindow.open(map, marker);
          };
    })
